4ff5d4df61513d7fcc0025a5b9ef6740e8fd1e9b
Denis Galkin <galkinsdenis@gmail.com>
2026-02-07 11:13:46 +0100

    V 1.0 BETA 9 -> Mobile adaptation

--- app.py
+++ app.py
@@ -1090,4 +1090,4 @@ def clear_notifications():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0',debug=True, port=5000)
